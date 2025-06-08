2b922ba809f6a60a75e4adb9441d442e4123e2a8
Paulo Tapia <pmtapia@espol.edu.ec>
2025-06-07 22:11:03 -0500

    Creacion de constructor en clase Foto
    Se creo el constructor de la clase Fotos

---
+++ src/main/java/com/espol/group04/grupo_04/Clases/Foto.java
@@ -0,0 +1,9 @@
+package com.espol.group04.grupo_04.Clases;
+
+public class Foto {
+    String numeroTelefono;
+
+    public Foto(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+}
